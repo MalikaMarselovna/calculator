8fb04ce64a4a796c8ea48dd9bf53177a3614fe88
MalikaMarselovna <mmukashova1@gmail.com>
2024-01-17 18:13:49 +0600

    connect js for calculator with +

--- core.js
+++ core.js
@@ -1,1 +1,9 @@
-console.log
+const input1 = $('.number-1')
+const input2 = $('.number-2')
+const btnPlus = $('.number-1')
+const result = $('.result')
+
+btnPlus.on('click', function(){
+    let sum =  +input1.val() + +input2.val()
+    result.text()
+})
